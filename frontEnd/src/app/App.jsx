b8c59866ffdfcd6a0359e4b526e6c237b06cfb43
Thurunu Hashitha <106402728+Thurunuhashitha@users.noreply.github.com>
2026-02-06 14:58:20 +0530

    first commit

---
+++ frontEnd/src/app/App.jsx
@@ -0,0 +1,18 @@
+import Dashboard from '../Pages/Dashboard/Dashboard'
+import './App.css'
+// import { Routes } from 'react-router-dom'
+// import { Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <div> 
+      <Dashboard/>
+      {/* <Routes>
+        <Route parth='/dashboard' element={<Dashboard/>}/>
+      </Routes> */}
+    </div>
+  )
+}
+
+export default App
